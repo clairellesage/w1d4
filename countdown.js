2c9fd1feb4869eb80811be440306248549dfd956
clairellesage <clairellesage@gmail.com>
2017-01-12 23:03:35 +0000

    decreasing|

--- countdown.js
+++ countdown.js
@@ -1,19 +1,26 @@
-var countdownGenerator = (function() {
-	var starting = 10;
-
-	var countdown = function() {
-		starting -= 1;
-		return starting;
+var countdownGenerator = function(startingVal) {
+	console.log("T-minus " + startingVal)
+	return function() {
+		startingVal -= 1;
+			if (startingVal >= 1){
+				console.log("T-minus " + startingVal)
+			} else if (startingVal === 0){
+				console.log("Blast off!")
+			} else {
+				console.log("Rocket's already gone")
+			}
+			return startingVal;
 	}
-})();
+}
 
-
-// var nextId = (function () {
-//   var id = 0;
-
-//   return function () {
-//     id += 1;
-//     return id;
+	
+// var loadedDie = (function () {
+//   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];	
+//   var result = 0;
+  
+//   return function() {
+//   	result += 1
+//   	return list[result - 1]
 //   }
 // })();
 
@@ -24,3 +31,9 @@ countdown(); // T-minus 1...
 countdown(); // Blast Off!
 countdown(); // Rockets already gone, bub!
 countdown(); // Rockets already gone, bub!
+
+
+console.log(countdownGenerator());  // 5
+
+
+
